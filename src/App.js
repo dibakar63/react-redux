e41cfaef05f229558c1f2ba466d5b7927fdbc349
dibakar63 <110836157+dibakar63@users.noreply.github.com>
2023-02-28 13:24:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+import Login from "./components/login";
+import Profile from "./components/profile";
+
+import ChangeColor from "./components/changeColor";
+
+function App() {
+  return (
+    <div className="App">
+      Hello
+      <Login />
+      <Profile />
+      <ChangeColor />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import userReducer from "./features/user";
+import themeReducer from "./features/theme";
+
+import App from "./App";
+
+const store = configureStore({
+  reducer: {
+    user: userReducer,
+    theme: themeReducer
+  }
+});
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
